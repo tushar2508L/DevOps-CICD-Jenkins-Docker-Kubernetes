b79df5c8386e295a90f6c22e9a52f980c957a041
Ubuntu <ubuntu@ip-172-31-1-227.ap-south-1.compute.internal>
2026-08-31 15:50:41 +0000

    Add React application

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h1>DevOps CI/CD Project</h1>
+
+      <p>
+        React Application deployed using Jenkins, Docker and Kubernetes.
+      </p>
+
+      <p>
+        GitHub → Jenkins → Docker → Docker Hub → Kubernetes
+      </p>
+    </div>
+  );
+}
+
+export default App;
